c995cbe50a68cdcf96a067ea5674afec48d0ee6a
HarjasBhatia <bhatiaharjassingh@gmail.com>
2025-10-28 21:42:39 +0530

    ques 35

---
+++ 590027010-harjas-018-q35.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void printFactors(int n) {
+    printf("Factors of %d: ", n);
+    
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    int num;
+    
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+    
+    if (num <= 0) {
+        printf("Please enter a positive integer.\n");
+        return 1;
+    }
+    
+    printFactors(num);
+    return 0;
+}
